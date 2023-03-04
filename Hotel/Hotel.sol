18c0d9dbe8a58a2da8bb3261092b2411c5cb5ed0
Jyotirmoy Barman <64324524+jyotirmoydotdev@users.noreply.github.com>
2023-03-04 18:28:12 +0530

    Hotel Booking Smart Contract Added

---
+++ Hotel/Hotel.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Hotel{
+    address payable public owner;
+    constructor (){
+        owner=payable(msg.sender);
+        currentstatus=Status.vacant;
+    }
+
+    enum Status {vacant,occupied}
+    Status currentstatus;
+
+    function RoomStatus()public view returns(string memory){
+        if (currentstatus==Status.vacant){
+            string memory message="The room is vacant";
+            return message;
+        }
+        else{
+            string memory message="The room is occupied";
+            return message;
+        }
+    }
+    event occupy(address _rep,uint amount);
+
+    struct BookingDetails{
+            address occupier;
+            uint value;
+    }
+    BookingDetails Occupier;
+
+    function GuestDetails()public view returns(BookingDetails memory){
+        return Occupier;
+    }
+    function BookRoom()payable public{
+        require(msg.value >=2 ether,"Not sufficent fund");
+        require(currentstatus==Status.vacant,"The room is occupied");
+
+        (bool sent, bytes memory data)=owner.call{value:msg.value}("");
+        require(true);
+
+        currentstatus=Status.occupied;
+        emit occupy(msg.sender,msg.value);
+
+        Occupier=BookingDetails(msg.sender,msg.value);
+    }
+
+    function CheckOut()public{
+        require(currentstatus==Status.occupied,"Your are checout of the room");
+        require(msg.sender==Occupier.occupier,"Your are not the occupier");
+        currentstatus=Status.vacant;
+    }
+}
